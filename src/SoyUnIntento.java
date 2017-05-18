30421c17e2f67fd8b4d46abbf04d5b340a08acb7
MrHugs <hsanchezs01ex@gmail.com>
2017-05-18 12:42:04 +0100

    mew

--- src/SoyUnIntento.java
+++ src/SoyUnIntento.java
@@ -3,5 +3,6 @@ public class SoyUnIntento {
 public static void main(String[] args) {
 	System.out.println("soy un syso");
 	System.out.println("hola hugo");
+	System.out.println("otro intento");
 }
 }
