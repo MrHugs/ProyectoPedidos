b5ba5f487e266437f7e360d3e7923c87bb2a1951
MrHugs <hsanchezs01ex@gmail.com>
2017-05-29 12:24:20 +0100

    eiii

--- src/test/testAlta.java
+++ src/test/testAlta.java
@@ -2,10 +2,20 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class testAlta {
 
+import modelo.Cliente;
+
+public class testAlta {
+	
+	
+	@Before
+	public void setUp() throws Exception {
+	Cliente cliente = new Cliente("1", "Manolo Pajares");
+	
+	}
 	@Test
 	public void testAltaArticulo() {
 		fail("Not yet implemented");
