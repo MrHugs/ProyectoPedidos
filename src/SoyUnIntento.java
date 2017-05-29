ff6518c1933fb2b664825cf7f60ee3242e1fa081
Fernan <fernanrsanchez@gmail.com>
2017-05-29 08:36:28 +0200

    Revert "primer commint and push Fernando"
    This reverts commit 26851c83d38ab78aee7ae684bf6fbadd27c2f140.
    
    # Conflicts:
    #	src/SoyUnIntento.java

--- src/SoyUnIntento.java
+++ src/SoyUnIntento.java
@@ -2,7 +2,10 @@
 public class SoyUnIntento {
 public static void main(String[] args) {
 	System.out.println("soy un syso");
+<<<<<<< HEAD
 	System.out.println("hola hugo");
 	System.out.println("otro intento");
+=======
+>>>>>>> parent of 26851c8... primer commint and push Fernando
 }
 }
