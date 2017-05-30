50ac067c6d270a73a8a803362cb5b0b67ae817f8
Fernan <fernanrsanchez@gmail.com>
2017-05-30 08:39:21 +0200

    envio ialtas

---
+++ src/control/adaptador/Altas.java
@@ -0,0 +1,33 @@
+package control.adaptador;
+
+import modelo.Articulo;
+import modelo.Cliente;
+import modelo.Pedido;
+import utiles.Tipo;
+
+public class Altas implements IAltas {
+
+	@Override
+	public boolean altaArticulo(Articulo articulo, Tipo tipo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean altaPedido(Pedido pedido, Tipo tipo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean altaCliente(Cliente cliente, Tipo tipo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
