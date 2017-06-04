f25b709841363e9bcec11d6b99465b06215acc30
MrHugs <hsanchezs01ex@gmail.com>
2017-06-04 20:33:32 +0100

    wtf

--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -3,7 +3,7 @@ package modelo;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Pedido {
+public class Pedido  implements Serializable{
 String id;
 Cliente cliente;
 ArrayList listaLineasPedidos = new ArrayList();
