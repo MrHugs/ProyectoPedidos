4e315371f3218858eda545af105af25ccc3d7ca5
MrHugs <hsanchezs01ex@gmail.com>
2017-06-07 18:37:19 +0100

    Vista de ConsultaCliente hecha

--- src/vista/UICliente.java
+++ src/vista/UICliente.java
@@ -10,12 +10,19 @@ import java.awt.GridBagLayout;
 import javax.swing.JTable;
 import java.awt.GridBagConstraints;
 import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class UICliente extends JFrame {
 
 	protected JPanel contentPane;
 	protected JTable tablaCliente;
 	protected JScrollPane scrollPane;
+	private JButton btnBuscar;
+	private JTextField textField;
+	private JLabel lblId;
 
 	
 
@@ -29,24 +36,49 @@ public class UICliente extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		
 		String[] nombrecolumnas = {"DNI","Nombre", "Descripción"};
 		Object[][] datos ={{"456365X", "SrNick","5"},{"456365X", "SrNick","7"}};
 		
+		lblId = new JLabel("Id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
 		scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 2;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 0;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 1;
 		contentPane.add(scrollPane, gbc_scrollPane);
 		tablaCliente = new JTable(datos,nombrecolumnas);
 		scrollPane.setViewportView(tablaCliente);
+		
+		btnBuscar = new JButton("Buscar");
+		GridBagConstraints gbc_btnBuscar = new GridBagConstraints();
+		gbc_btnBuscar.gridx = 1;
+		gbc_btnBuscar.gridy = 3;
+		contentPane.add(btnBuscar, gbc_btnBuscar);
 	}
 
 }
